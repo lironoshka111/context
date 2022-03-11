79c1c43bf84dc5d91f5b00a0fbe7b7f65ba888dc
lironoshka111 <46004238+lironoshka111@users.noreply.github.com>
2022-03-11 16:53:14 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import ReactDOM from "react-dom";
+import Name from "./NameComponent";
+import Location from "./LocationComponent";
+import UserForm from "./UserForm";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className="is-size-4">
+        We'll use the React Context API to pass and receive data in any
+        component.
+      </h2>
+      <div className="container">
+        <UserForm />
+
+        <h2 className="is-size-4">Display User Info Here 👇</h2>
+        <p>
+          These two children components will receive data. These could be nested
+          components.
+        </p>
+        <Name />
+        <Location />
+      </div>
+    </div>
+  );
+}
+
+// Wrap parent component with context provider
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
